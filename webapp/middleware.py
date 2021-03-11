33bec07cec823329e021b393527051dd5448e0e2
mustafaali96 <ssuet.se2016@gmail.com>
2021-03-11 16:02:50 +0500

    middleware

--- webapp/middleware.py
+++ webapp/middleware.py
@@ -11,20 +11,3 @@ class MyMiddleware:
         response["Access-Control-Allow-Headers"] = "Origin,Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,locale"
         response["Access-Control-Allow-Methods"] = "GET, POST, PUT, PATCH, DELETE, OPTIONS"
         return response
-
-   
-
-#     def list(self, request, *args, **kwargs):
-#         queryset = self.filter_queryset(self.get_queryset())
-#         page = self.paginate_queryset(queryset)
-#         if page is not None:
-#             serializer = self.get_serializer(page, many=True)
-#             return self.get_paginated_response(serializer.data)
-#         serializer = self.get_serializer(queryset, many=True)
-#         headers = {}
-#         headers["Access-Control-Allow-Origin"] = "*"
-#         headers["Access-Control-Allow-Credentials"] = True
-#         headers["Access-Control-Allow-Headers"] = "Origin,Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,locale"
-#         headers["Access-Control-Allow-Methods"] = "GET, POST, OPTIONS"
-#         return Response(data=serializer.data, headers=headers)
-        
