b957093286e91569c17824d5debd011360181762
mustafaali96 <ssuet.se2016@gmail.com>
2021-05-26 12:19:01 +0500

    platform type

--- webapp/serializer/platform_serializer.py
+++ webapp/serializer/platform_serializer.py
@@ -1,12 +1,7 @@
 from rest_framework import serializers
-
-from webapp.serializer.platform_type_serializer import PlatformTypeSerializer
 from webapp import models
 
 class PlatformSerializer(serializers.ModelSerializer):
-    
-    platform_type = PlatformTypeSerializer()
- 
     class Meta:
         model = models.Platform
         fields = "__all__"
